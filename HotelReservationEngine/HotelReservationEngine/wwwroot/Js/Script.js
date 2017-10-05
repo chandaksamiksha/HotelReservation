8b54237f41c6a6d0959f856f99150791a09a9c62
Samiksha Chandak <schandak@tavisca.com>
2017-10-05 18:52:55 +0530

    complete search form with validations

--- HotelReservationEngine/HotelReservationEngine/wwwroot/Js/Script.js
+++ HotelReservationEngine/HotelReservationEngine/wwwroot/Js/Script.js
@@ -1,1 +1,13 @@
-﻿
+﻿//Data Extraction Function
+function extractData() {
+    var place = $("#place")[0].value;
+    var checkInDate = $("#datePickerId1")[0].value;
+    var checkOutDate=$("#datePickerId2")[0].value;
+    var adultNumber = $("#adult")[0].value;
+    var childNumber=$("#children")[0].value;
+    $.ajax({
+        type: "GET",
+        url: "guestDetails/",
+        success: function (addElement) { $("#addElement").html(addElement); }
+    })
+};
